631d9d7dc9f944f5abb790d588623659ba27f51f
kamail12 <kamiloxen1@gmail.com>
2023-07-14 10:33:03 +0200

    Added logger middlewear

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
 // import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+const logger = store => next => action => {
+	console.group(action.type);
+	console.info("dispatching", action);
+	let result = next(action);
+	console.log("next state", store.getState());
+	console.groupEnd();
+
+	return result;
+};
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
